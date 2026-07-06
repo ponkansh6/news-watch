f2257eb06dfc820816d28efb29206f76f5bb37b3
shunki <shunki@users.noreply.github.com>
2026-07-06 21:05:40 +0900

    feat: add ITmedia to UI sources

--- src/app/fetch-button.tsx
+++ src/app/fetch-button.tsx
@@ -17,6 +17,7 @@ const SOURCES = [
   { id: "qiita", name: "Qiita", color: "bg-purple-500" },
   { id: "github", name: "GitHub", color: "bg-gray-700" },
   { id: "yamadashy", name: "Tech Blog", color: "bg-emerald-500" },
+  { id: "itmedia", name: "ITmedia", color: "bg-red-500" },
 ];
 
 export default function FetchButton() {
